bb9800cde01557ee0d62339a92225fd1fed90af4
Mititelu Cristina <mititelucristina29@gmail.com>
2022-04-10 19:40:39 +0300

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,59 @@
-import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BaseRegisterComponent } from './components/register/base-register/base-register.component';
+import { ChooseLoginTypeComponent } from './components/login/choose-login-type/choose-login-type.component';
+import { FarmacyDashboardComponent } from './components/dashboards/farmacy-dashboard/farmacy-dashboard/farmacy-dashboard.component';
+import { LoginComponent } from './components/login/login/login.component';
+import { MedicDashboardComponent } from './components/dashboards/medic-dashboard/medic-dashboard/medic-dashboard.component';
+import { NgModule } from '@angular/core';
+import { PacientDashboardComponent } from './components/dashboards/pacient-dashboard/pacient-dashboard/pacient-dashboard.component';
+import { SpecialistDashboardComponent } from './components/dashboards/specialist-dashboard/specialist-dashboard/specialist-dashboard.component';
+
+const routes: Routes = [
+  // login
+  {
+    path: '',
+    component: ChooseLoginTypeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  // register
+  {
+    path: 'register-medic',
+    component: BaseRegisterComponent
+  },
+  {
+    path: 'register-specialist',
+    component: BaseRegisterComponent
+  },
+  {
+    path: 'register-pacient',
+    component: BaseRegisterComponent
+  },
+  {
+    path: 'register-farmacy',
+    component: BaseRegisterComponent
+  },
+  // dashboards
+  {
+    path: 'medic-dashboard',
+    component: MedicDashboardComponent
+  },
+  {
+    path: 'specialist-dashboard',
+    component: SpecialistDashboardComponent
+  },
+  {
+    path: 'pacient-dashboard',
+    component: PacientDashboardComponent
+  },
+  {
+    path: 'farmacy-dashboard',
+    component: FarmacyDashboardComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
