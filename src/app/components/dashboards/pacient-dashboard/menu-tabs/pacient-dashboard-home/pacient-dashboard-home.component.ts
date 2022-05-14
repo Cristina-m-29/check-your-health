659667b2998c085d8795edda3ca81f89805fd8d6
Cristina Mititelu <mititelucristina29@gmail.com>
2022-05-14 13:53:28 +0300

    added loading screen

--- src/app/components/dashboards/pacient-dashboard/menu-tabs/pacient-dashboard-home/pacient-dashboard-home.component.ts
+++ src/app/components/dashboards/pacient-dashboard/menu-tabs/pacient-dashboard-home/pacient-dashboard-home.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'cyh-pacient-dashboard-home',
   templateUrl: './pacient-dashboard-home.component.html',
   styleUrls: ['./pacient-dashboard-home.component.sass']
 })
-export class PacientDashboardHomeComponent implements OnInit {
+export class PacientDashboardHomeComponent {
+  public loading = true;
 
-  constructor() { }
+  private gotAppointments = false;
+  private gotMedic = false;
 
-  ngOnInit() {
+  public doneWithGettingAppointments(): void {
+    this.gotAppointments = true;
+    this.checkIfLoadingIsDone();
   }
 
+  public doneWithGettingMedic(): void {
+    this.gotMedic = true;
+    this.checkIfLoadingIsDone();
+  }
+
+  private checkIfLoadingIsDone(): void {
+    if (this.gotAppointments && this.gotMedic) {
+      this.loading = false;
+    }
+  }
 }
