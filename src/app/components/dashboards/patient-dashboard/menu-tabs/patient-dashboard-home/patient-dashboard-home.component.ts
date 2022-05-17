ebd3e5e09c27d221d515e6daf3417c26d110a24a
Cristina Mititelu <mititelucristina29@gmail.com>
2022-05-17 14:11:52 +0300

    added loading to appointments page

--- src/app/components/dashboards/patient-dashboard/menu-tabs/patient-dashboard-home/patient-dashboard-home.component.ts
+++ src/app/components/dashboards/patient-dashboard/menu-tabs/patient-dashboard-home/patient-dashboard-home.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cyh-patient-dashboard-home',
   templateUrl: './patient-dashboard-home.component.html',
   styleUrls: ['./patient-dashboard-home.component.sass']
 })
-export class PatientDashboardHomeComponent {
-  // public loading = true;
-  public loading = false;
+export class PatientDashboardHomeComponent implements OnInit{
+  public loading = true;
 
-  private gotAppointments = false;
-  private gotMedic = false;
+  private gotAppointments = true;
+  private gotMedic = true;
+
+
+  public ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 800);
+  }
 
   public doneWithGettingAppointments(): void {
     this.gotAppointments = true;
