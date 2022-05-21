9578e1cce8e1d339f7a25ea05c9a93617ae589d5
Radu Florin Marinoiu <radumarinoiu98@gmail.com>
2022-05-21 18:35:11 +0300

    Removed all logging from app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
-import * as moment from 'moment';
 import { filter } from 'rxjs';
 import { options } from './models/menu';
 
@@ -20,12 +19,5 @@ export class AppComponent implements OnInit {
       .subscribe((event: any) => {
         this.showMainNavbar = event.url.includes('dashboard') || event.url.includes('cont') || options.menuList.find(option => event.url.includes(option));
       });
-    // console.log(moment.utc().unix());
-    // console.log(moment.utc().hours(8).unix());
-    // const date = moment.unix(1652400000);
-    // console.log(date);
-    // console.log(date.hours(8).minutes(0).seconds(0).milliseconds(0));
-    // console.log(date.toLocaleString());
-    // console.log(moment().toLocaleString())
   }
 }
