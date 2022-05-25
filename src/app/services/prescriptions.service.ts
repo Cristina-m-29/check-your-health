aedf1cb8b3ad522b1af9adb15fa6d55c88af9469
Radu Florin Marinoiu <radumarinoiu98@gmail.com>
2022-05-25 20:33:30 +0300

    Added PrescriptionService (GET, POST) and added 'id' to medicine model

---
+++ src/app/services/prescriptions.service.ts
@@ -0,0 +1,34 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import * as moment from 'moment';
+import { parse } from 'path';
+import { catchError, EMPTY, Observable } from 'rxjs';
+import { Medicine } from '../models/medicine';
+import { Prescription } from '../models/prescription';
+import { BaseService } from './base.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PrescriptionsService {
+
+  constructor(private baseService: BaseService) { }
+
+  public getPrescriptions(): Observable<Prescription[]> {
+    return this.baseService.get<Prescription[]>('users/prescriptions');
+  }
+
+  public addPrescription(patientId: string, pharmacyId: string, date: Date, medicines: Medicine[]): Observable<Prescription> {
+    const parsedDate = moment(date).utcOffset(0, true).unix();
+    return this.baseService.post<any, Prescription>('users/prescriptions', {
+      patient: patientId,
+      pharmacy: pharmacyId,
+      date: parsedDate,
+      medicines: medicines
+    }).pipe(catchError((err: HttpErrorResponse) => {
+      console.log(err);
+      return EMPTY;
+    }));
+  }
+
+}
